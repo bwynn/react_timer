031ad994bf3c8c387393cb1e327f8cb7b053e92b
Brian Wynn <b_wynn@apple.com>
2017-01-11 16:01:19 -0800

    Configured test suite.

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+const webpackConfig = require('./webpack.config.js');
+
+// Karma configuration
+// Generated on Wed Jan 11 2017 15:49:20 GMT-0800 (PST)
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: ['app/tests/**/*.test.jsx'],
+    preprocessors: {
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap'],
+    },
+    reporters: ['mocha'],
+    client: {
+      mocha: {
+        timeout: '5000',
+      },
+    },
+    webpack: webpackConfig,
+  });
+};
