009d32e64a623279bca4a25a7c3f887e5c7f2594
Brian Wynn <b_wynn@apple.com>
2017-01-11 17:02:48 -0800

    Defined initial component tests for clock component. Added styles to component.

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 const React = require('react');
+const Clock = require('Clock');
 
 const Countdown = React.createClass({
   render: function () {
     return (
       <div>
-        <p>Countdown.jsx</p>
+        <Clock totalSeconds={129}/>
       </div>
     );
   },
