b9dfed1fc5e2bf35adf75d2eb774718fedac095a
Brian Wynn <b_wynn@apple.com>
2017-01-13 16:12:33 -0800

    Added tests to countdown component.

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,39 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const expect = require('expect');
+const $ = require('jQuery');
+const TestUtils = require('react-addons-test-utils');
+
+const Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown()', () => {
+    it('should set state to started and count down', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done(); // required param for mocha to handle async test.
+      }, 1001);
+    });
+
+    it('should stop counting at 0', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done(); // required param for mocha to handle async test.
+      }, 3001);
+    });
+
+  });
+});
